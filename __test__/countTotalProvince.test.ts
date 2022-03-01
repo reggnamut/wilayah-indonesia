f2063e16bf86862341ec6b6b56ef7e8864a16fb4
rzkytmgr <rzkytmgr@gmail.com>
2022-03-01 21:42:37 +0700

    add(unit testing): add unit testing

---
+++ __test__/countTotalProvince.test.ts
@@ -0,0 +1,10 @@
+import { countTotal } from "../src/v1/lib/helper";
+
+describe("Count total provinces", () => {
+  it("If the data length is zero", () => {
+    expect(countTotal([])).toBe(0);
+  });
+  it("If there is a data or more", () => {
+    expect(countTotal([1, 2, 3])).toBe(3);
+  });
+});
